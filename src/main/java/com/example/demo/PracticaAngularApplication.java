332371652a7c75d2228d834960993418dc5cde8f
codegaws <codegaws@gmail.com>
2023-11-04 11:55:37 -0500

    first R

---
+++ src/main/java/com/example/demo/PracticaAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaAngularApplication.class, args);
+	}
+
+}
